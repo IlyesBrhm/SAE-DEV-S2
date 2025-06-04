1fd7fbb619fe387a6507b3505fab7884df11a830
aymc93 <aclaudio@iut.univ-paris8.fr>
2025-06-04 10:33:26 +0200

    carft + reglage + nettoyage
    reprise du code de craft avec séparation coeur et bouclier, reglage du probleme pour poser des bloc sur le player et nettoyage des methode inutile

--- src/main/java/universite_paris8/iut/youadah/projet/modele/Pioche.java
+++ src/main/java/universite_paris8/iut/youadah/projet/modele/Pioche.java
@@ -4,12 +4,12 @@ import javafx.scene.layout.TilePane;
 import universite_paris8.iut.youadah.projet.vue.MapVue;
 
 public class Pioche extends Objet{
-    Map carte;
+    GameMap carte;
     MapVue carteVue;
     Player joueur;
     TilePane tileMap;
     
-    public Pioche(String nom, int rarete, Map carte, MapVue carteVue, Player joueur, TilePane tileMap) {
+    public Pioche(String nom, int rarete, GameMap carte, MapVue carteVue, Player joueur, TilePane tileMap) {
         super(nom, rarete, false);
         this.carte = carte;
         this.carteVue = carteVue;
